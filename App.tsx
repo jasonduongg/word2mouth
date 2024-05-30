883007628dcec33ba460dcd77b7748914ae7900e
jasonduong-portfolio <jasonduong.website@gmail.com>
2024-05-30 01:24:29 -0700

    scroll function

--- App.tsx
+++ App.tsx
@@ -1,38 +1,45 @@
-import React, { useState } from 'react';
-import { SafeAreaView, TextInput, Button, View, Text, Alert } from 'react-native';
-import { getDatabase, ref, set } from 'firebase/database';
-import { db } from './components/config'; // Ensure this import points correctly to your Firebase config
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import Scroller from './components/Scroller/Scroller.tsx'
 
 const App = () => {
-  const [lobbyName, setLobbyName] = useState("fuck text");
-
-  const setValueInDB = () => {
-    if (!lobbyName.trim()) {
-      Alert.alert('Validation Error', 'Lobby name is required!');
-      return;
-    }
-    // Define the reference path to the value you want to set
-    const databaseRef = ref(db, `/lobbies/${lobbyName}`);
-    // Set the value in the database
-    set(databaseRef, { name: lobbyName })
-      .then(() => Alert.alert('Success', 'Lobby created successfully!'))
-      .catch(error => Alert.alert('Error', error.message));
-  };
 
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Enter the name of the lobby:</Text>
-      <TextInput
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 200, marginBottom: 10 }}
-        onChangeText={setLobbyName}
-        value={lobbyName}
-      />
-      <Button
-        title="Create Lobby"
-        onPress={setValueInDB}
-      />
-    </SafeAreaView>
+    <View style={styles.appContainer}>
+      <View style={styles.container}>
+        <Scroller />
+      </View>
+      <View style={styles.navbar}>
+        <Text style={styles.navbarText}>Navbar</Text>
+        <Text style={styles.navbarText}>Navbar</Text>
+
+        <Text style={styles.navbarText}>Navbar</Text>
+
+      </View>
+    </View>
   );
 };
 
-export default App
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+  container: {
+    height: '92%',
+  },
+  navbar: {
+    height: '8%',
+    backgroundColor: '#0e1111',
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+  },  
+  navbarText: {
+    color: 'white',
+    marginRight: 5,
+    marginLeft: 5,
+  }  
+});
+
+export default App;
